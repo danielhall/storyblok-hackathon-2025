0047429a3f1c7dca7c2ff80f98bb8440e98300a0
Ben Haynes <ben.haynes@ridgeway.com>
2025-07-01 14:34:53 +0100

    add vnav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,26 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import { IoIosHome } from 'react-icons/io';
+import './globals.css';
 
-import StoryblokProvider from "@/components/StoryblokProvider";
+import StoryblokProvider from '@/components/StoryblokProvider';
+
+import Link from 'next/link';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Storyblok IA Dashboard",
-  description: "Visualise and manage your Storyblok Information Architecture with live tree view, OpenAI-powered redirect suggestions, and modern UX.",
+  title: 'Storyblok IA Dashboard',
+  description:
+    'Visualise and manage your Storyblok Information Architecture with live tree view, OpenAI-powered redirect suggestions, and modern UX.',
 };
 
 export default function RootLayout({
@@ -25,11 +29,29 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <StoryblokProvider> 
-      <html lang="en">
+    <StoryblokProvider>
+      <html lang='en'>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <div className='flex items-center gap-x-6 py-2 px-[15px]'>
+            <Link href='/'>
+              <IoIosHome
+                style={{
+                  height: 20,
+                  width: 20,
+                }}
+              />
+            </Link>
+
+            <Link className='mt-0.5' href='/dashboard'>
+              Dashboard
+            </Link>
+            <Link className='mt-0.5' href='/sitemap-visualiser'>
+              Visualiser
+            </Link>
+          </div>
+
           {children}
         </body>
       </html>
